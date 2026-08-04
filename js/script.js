b41bb9200a7133060f2bc1b3a9cc2bcc45c394ae
Jmike-dev <gmikekamau@gmail.com>
2026-08-04 15:07:54 +0300

    feat:created an object with the image and its data

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const destinations = [
+    {
+        image:"/images/Image1.png",
+        location:"London, Uk",
+        price:"$4.2k",
+        trip:"12"
+    },
+        {
+        image:"/images/Image2.png",
+        location:"London, Uk",
+        price:"$4.2k",
+        trip:"12"
+    },
+        {
+        image:"/images/Image3.png",
+        location:"Full Europe",
+        price:"$4.2k",
+        trip:"28"
+    },
+]
