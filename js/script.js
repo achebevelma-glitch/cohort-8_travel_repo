8dc5b1ade78bd056dc15e7ae5facd60885d36bbe
Jmike-dev <gmikekamau@gmail.com>
2026-08-06 11:48:45 +0300

    feat:still working on the UI

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@ const destinations = [
     {
         image:"images/image1.png",
         location:"London, Uk",
-        price:"$4.2k",
+        price:"$ 4.2k",
         trip:"12"
     },
         {
         image:"images/image2.jpg",
         location:"London, Uk",
-        price:"$4.2k",
+        price:"$ 4.2k",
         trip:"12"
     },
         {
         image:"images/image3.png",
         location:"Full Europe",
-        price:"$4.2k",
+        price:"$ 4.2k",
         trip:"28"
     },
 ]
@@ -27,9 +27,9 @@ container.innerHTML = destinations
             <img src="${dest.image}" alt="${dest.location}" class="card-image">
             <div class="card-body">
                 <h3 class="card-location">${dest.location}</h3>
-                <p class="card-price">${dest.price} <span>/ person</span></p>
-                <p class="card-trip">${dest.trip} Trips available</p>
-            </div>
+                <h3 class="card-price">${dest.price}</3>
+                </div>
+                <p class="card-trip" >${dest.trip} Trip</p>
         </div>
     `
     )
